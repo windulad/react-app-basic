e0b88c32062fa00f5361834486624473bcec0079
windulad <dissana-cs19010@stu.kln.ac.lk>
2023-04-08 09:17:24 +0530

    commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const main = {
+  background: "cyan",
+  height: "100vh",
+  width: "100%", 
+};
+
+const firstvariable = "Expert";
+const secondvariable = "Developer";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Hello World</h1>
-  </React.StrictMode> 
+  <>
+    <div style={main}>
+      <h1>Hey, What's up!</h1>
+      <h1>{`We are ${firstvariable} ${secondvariable}`}</h1>
+    </div>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
